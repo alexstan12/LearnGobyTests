1330443265843ea735377bba5218fac4ac979540
alex <alexandrustan96@yahoo.ro>
2021-10-23 21:01:40 +0300

    finished mocking chapter

--- mocking/mocking.go
+++ mocking/mocking.go
@@ -3,6 +3,7 @@ package mocking
 import (
 	"fmt"
 	"io"
+	"time"
 )
 
 func Countdown(w io.Writer, sleeper Sleeper){
@@ -25,3 +26,43 @@ type SpySleeper struct {
 func (s *SpySleeper) Sleep(){
 	s.Calls++
 }
+
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+func (s *SpyCountdownOperations) Sleep(){
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error){
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep func(duration time.Duration)
+}
+
+func (c *ConfigurableSleeper) SetDuration(d time.Duration){
+	c.duration = d
+}
+func (c *ConfigurableSleeper) SetSleepFunc(f func(d time.Duration)){
+	c.sleep = f
+}
+
+func (c *ConfigurableSleeper) Sleep(){
+	c.sleep(c.duration)
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration){
+	s.durationSlept = duration
+}
+
+const write = "write"
+const sleep = "sleep"
