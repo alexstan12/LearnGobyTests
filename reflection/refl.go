7d5f231ea761194f2e05f5c3032b7811f1d304e2
alex <alexandrustan96@yahoo.ro>
2021-11-27 14:37:27 +0200

    finished test with reflection on all usual types

--- reflection/refl.go
+++ reflection/refl.go
@@ -1,9 +1,49 @@
 package reflection
 
-import "reflect"
+import (
+	"reflect"
+)
+
+func getValue(x interface{}) reflect.Value {
+	val := reflect.ValueOf(x)
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+	return val
+}
 
 func walk(x interface{}, fn func(string)) {
-	val := reflect.ValueOf(x)
-	field := val.Field(0)
-	fn(field.String())
+	val := getValue(x)
+
+	walkValue := func(value reflect.Value) {
+		walk(value.Interface(), fn)
+	}
+
+	switch val.Kind() {
+	case reflect.String:
+		fn(val.String())
+	case reflect.Struct:
+		for i := 0; i< val.NumField(); i++ {
+			walkValue(val.Field(i))
+		}
+	case reflect.Slice, reflect.Array:
+		for i:= 0; i<val.Len(); i++ {
+			walkValue(val.Index(i))
+		}
+	case reflect.Map:
+		for _, key := range val.MapKeys() {
+			walkValue(val.MapIndex(key))
+		}
+	case reflect.Chan:
+		for v, ok:= val.Recv();ok; v,ok = val.Recv(){
+			walkValue(v)
+		}
+	case reflect.Func:
+		valFnResult := val.Call(nil)
+		for _, res := range valFnResult {
+			walkValue(res)
+		}
+
+	}
+
 }
